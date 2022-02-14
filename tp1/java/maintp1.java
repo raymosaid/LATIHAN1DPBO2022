2f52fd0ec542c8fc3d2629b43d54c0bce0862765
raymosaid <99600360+raymosaid@users.noreply.github.com>
2022-02-14 23:55:05 +0700

    Update maintp1.java

--- tp1/java/maintp1.java
+++ tp1/java/maintp1.java
@@ -8,20 +8,24 @@ import java.util.*;
 
 public class maintp1 {
     public static void main(String[] args){
+        //membuat variabel untuk menampung masukan user
         String team;
         String country;
         String est;
         String playername;
 
+        //meminta masukan user untuk mengisi class team 1
         Scanner sc = new Scanner(System.in);
         team = sc.nextLine();
         country = sc.nextLine();
         est = sc.nextLine();
         playername = sc.nextLine();
 
+        //mengisi class team1
         classtp1 team1 = new classtp1(team, country, est, playername);
 
-        System.ouat.println("Team Name: " + team1.getteam());
+        //menampilkan class team1
+        System.out.println("Team Name: " + team1.getteam());
         System.out.println("Country: " + team1.getcountry());
         System.out.println("Established: " + team1.getest());
         System.out.println("Player: " + team1.getplayer());
